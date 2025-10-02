d0e2f60d5cd40caea3f6d6daa1e422c793813267
PowerShell Universal <PowerShell Universal>
2025-10-02 16:51:20 +0200

    Updating via git sync.

---
+++ .universal/authentication.ps1
@@ -0,0 +1,12 @@
+﻿Set-PSUAuthenticationMethod -Type "Form" -ScriptBlock {
+    param(
+        [PSCredential]$Credential
+    )
+
+    #
+    #   You can call whatever cmdlets you like to conduct authentication here.
+    #   Just make sure to return the $Result with the Success property set to $true
+    #
+
+    New-PSUAuthenticationResult -ErrorMessage 'Bad username or password2'
+}
