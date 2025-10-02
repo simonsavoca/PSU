79df3ffb9c0d63fa718c089a6d321a1db1011b31
PowerShell Universal <PowerShell Universal>
2025-10-02 19:00:28 +0200

    Updating via git sync.

--- .universal/endpoints.ps1
+++ .universal/endpoints.ps1
@@ -1,3 +1,12 @@
 ﻿New-PSUEndpoint -Url "/hello-world" -Description "test" -Method @('GET') -Endpoint {
+    <# 
+.SYNOPSIS
+This is an endpoint to get computer info
+
+.DESCRIPTION
+Get detailed computer info
+#>
+
     # Enter your script to process requests.
+    Get-ComputerInfo
 } -Documentation "Hello-World"
