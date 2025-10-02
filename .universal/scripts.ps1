1e16489916a50961f008dce264b48ed94a7fbe0b
PowerShell Universal <PowerShell Universal>
2025-10-02 17:57:18 +0200

    Updating via git sync.

--- .universal/scripts.ps1
+++ .universal/scripts.ps1
@@ -1,3 +1,4 @@
 ﻿New-PSUScript -Name "ActiveDirectory-Connect.ps1" -Description "ActiveDirectory-Connect.ps1" -Path "ActiveDirectory-Connect.ps1" 
 New-PSUScript -Name "HelloWorld.ps1" -Description "HelloWorld.ps1" -Path "HelloWorld.ps1" 
+New-PSUScript -Environment "PowerShell 7" -Module "PowerShellUniversal.Apps.AutomatedLab" -Command "Start-Lab" 
 New-PSUScript -Environment "PowerShell 7" -Module "PowerShellUniversal.Apps.AutomatedLab" -Command "Start-Lab"
