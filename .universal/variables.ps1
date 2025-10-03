ab6182056666edb47a1afca9615e4964a211ef87
PowerShell Universal <PowerShell Universal>
2025-10-03 12:36:37 +0200

    Updating via git sync.

--- .universal/variables.ps1
+++ .universal/variables.ps1
@@ -1,2 +1,3 @@
-﻿New-PSUVariable -Name "Duckplatform" -Vault "Database" 
+﻿New-PSUVariable -Name "Domains" -Value @('duckplatform.local') -Type "System.Collections.ArrayList" 
+New-PSUVariable -Name "Duckplatform" -Vault "Database" 
 New-PSUVariable -Name "MyVariable" -Value 'sdsqdsqdqd' -Description "sdqsdqsd"
--- ReadSecret.ps1
+++ ReadSecret.ps1
@@ -1,2 +1,6 @@
 ﻿# Script contents
 Write-Output $Secret:Duckplatform
+Write-Output $MyVariable
+Foreach ($Domain in $Domains) {
+    Write-Output $Domain
+}
