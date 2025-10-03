ba571ee110f90aafbf69799dfba04bbca123a44a
PowerShell Universal <PowerShell Universal>
2025-10-03 17:18:46 +0200

    Updating via git sync.

--- .universal/variables.ps1
+++ .universal/variables.ps1
@@ -1,5 +1,5 @@
-﻿New-PSUVariable -Name "Domains" -Value @('duckplatform.local') -Type "System.Collections.ArrayList" 
+﻿New-PSUVariable -Name "Admin" -Vault "Database" -Type "PSCredential" 
+New-PSUVariable -Name "Domains" -Value @('duckplatform.local') -Type "System.Collections.ArrayList" 
 New-PSUVariable -Name "Duckplatform" -Vault "Database" 
 New-PSUVariable -Name "Labsources" -Value 'c:\Labsources' 
-New-PSUVariable -Name "MyVariable" -Value 'sdsqdsqdqd' -Description "sdqsdqsd" 
-New-PSUVariable -Name "Variable" -Vault "Database" -Type "PSCredential"
+New-PSUVariable -Name "MyVariable" -Value 'sdsqdsqdqd' -Description "sdqsdqsd"
