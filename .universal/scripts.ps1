3ef5ad9e71377cc8bd59183d122c73f45f47c2c3
PowerShell Universal <PowerShell Universal>
2025-10-03 17:22:56 +0200

    Updating via git sync.

--- .universal/scripts.ps1
+++ .universal/scripts.ps1
@@ -7,5 +7,5 @@ New-PSUScript -Environment "PowerShell 7" -Module "PowerShellUniversal.Apps.Auto
 New-PSUScript -Environment "PowerShell 7" -Module "PowerShellUniversal.Apps.AutomatedLab" -Command "Start-Lab" 
 New-PSUScript -Environment "PowerShell 7" -Module "PowerShellUniversal.Apps.AutomatedLab" -Command "Start-Lab" 
 New-PSUScript -Environment "PowerShell 7" -Module "PowerShellUniversal.Apps.AutomatedLab" -Command "Start-Lab" 
-New-PSUScript -Name "PSU-Lab.ps1" -Description "PSU-Lab.ps1" -Path "PSU-Lab.ps1" -Credential "Variable" 
+New-PSUScript -Name "PSU-Lab.ps1" -Description "PSU-Lab.ps1" -Path "PSU-Lab.ps1" -Environment "Windows PowerShell 5.1" -Credential "Admin" 
 New-PSUScript -Name "ReadSecret.ps1" -Description "ReadSecret.ps1" -Path "ReadSecret.ps1"
