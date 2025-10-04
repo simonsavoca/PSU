eff4ae17fe926161815af8fcc7f11242289e153e
admin <admin>
2025-10-04 12:01:10 +0200

    Edited: ActiveDirectory-Connect.Tests.ps1

--- ActiveDirectory-Connect.Tests.ps1
+++ ActiveDirectory-Connect.Tests.ps1
@@ -3,6 +3,6 @@ Describe 'ActiveDirectory-Connect.' {
     It 'Job' {
         Invoke-PSUScript -Script 'ActiveDirectory-Connect.ps1' -RequiredParameter 'Hello' | Tee-Object -Variable job | Wait-PSUJob
         $Output = Get-PSUJobPipelineOutput -Job $Job
-        Write-Output $Output
+        $Output | Should -Not -BeNullOrEmpty
     }
 }
