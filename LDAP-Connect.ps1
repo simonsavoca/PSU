fcbdea050c00fc9b57029c53e667f84a0a3a7473
PowerShell Universal <PowerShell Universal>
2025-10-04 10:59:31 +0200

    Updating via git sync.

--- LDAP-Connect.ps1
+++ LDAP-Connect.ps1
@@ -1,7 +1,9 @@
 ﻿# Script contents
 #Param([String]$Domain)
 
-$DomainObj = Get-ADDomain -Server 192.168.11.10 -Credential $Secret:DuckAdmin
+#$DomainObj = Get-ADDomain -Server 192.168.11.10 -Credential $Secret:DuckAdmin
 
-$ADSI = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$($DomainObj.DistinguishedName)", $Secret:DuckAdmin.UserName, $Secret:DuckAdmin.Password)
-Write-Output $ADSI
+#$ADSI = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$($DomainObj.DistinguishedName)", $Secret:DuckAdmin.UserName, $Secret:DuckAdmin.Password)
+
+$Connection = [adsi]"LDAP://192.168.11.10:389"
+Write-Output $Connection
