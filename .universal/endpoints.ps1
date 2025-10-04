1f996675b117888edc6e0a808fc7a8a8c8e6f202
PowerShell Universal <PowerShell Universal>
2025-10-04 11:36:56 +0200

    Updating via git sync.

--- .universal/endpoints.ps1
+++ .universal/endpoints.ps1
@@ -1,4 +1,4 @@
-﻿New-PSUEndpoint -Url "/inactiveusers" -Description "Get Inactive Users of a domain" -Method @('GET') -Endpoint {
+﻿New-PSUEndpoint -Url "/inactiveusers/:domain" -Description "Get Inactive Users of a domain" -Method @('GET') -Endpoint {
     # Enter your script to process requests.
     "Hello world"
 } -Authentication -Documentation "Docs"
