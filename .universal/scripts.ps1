7afe8e9ff80ca1a14f3932c55c5d533dc85554d8
PowerShell Universal <PowerShell Universal>
2025-10-04 10:51:16 +0200

    Updating via git sync.

--- .universal/scripts.ps1
+++ .universal/scripts.ps1
@@ -1,4 +1,4 @@
-﻿New-PSUScript -Name "ActiveDirectory-Connect.ps1" -Description "ActiveDirectory-Connect.ps1" -Path "ActiveDirectory-Connect.ps1" 
+﻿New-PSUScript -Name "ActiveDirectory-Connect.ps1" -Description "ActiveDirectory-Connect.ps1" -Path "ActiveDirectory-Connect.ps1" -Credential "DuckAdmin" 
 New-PSUScript -Name "HelloWorld.ps1" -Description "HelloWorld.ps1" -Path "HelloWorld.ps1" 
 New-PSUScript -Name "LDAP-Connect.ps1" -Description "LDAP-Connect.ps1" -Path "LDAP-Connect.ps1" 
 New-PSUScript -Environment "PowerShell 7" -Module "PowerShellUniversal.Apps.AutomatedLab" -Command "Start-Lab" 
--- ActiveDirectory-Connect.ps1
+++ ActiveDirectory-Connect.ps1
@@ -1,3 +1,4 @@
 ﻿# Script contents
-Get-ADUser -Filter * -Server 192.168.11.10 -Credential $Secret:DuckAdmin
-Get-ADGroup -Filter * -Server 192.168.11.10 -Credential $Secret:DuckAdmin
+#Get-ADUser -Filter * -Server 192.168.11.10 -Credential $Secret:DuckAdmin
+#Get-ADGroup -Filter * -Server 192.168.11.10 -Credential $Secret:DuckAdmin
+Get-ADUser -Filter * -Server 192.168.11.10
