99e71e2c92055af1c3d867530fb19faed199fdbe
admin <admin>
2025-10-04 12:41:42 +0200

    It works !

--- ActiveDirectory-Connect.ps1
+++ ActiveDirectory-Connect.ps1
@@ -3,3 +3,4 @@ Get-ADUser -Filter * -Server 192.168.11.10 -Credential $Secret:DuckAdmin
 #Get-ADGroup -Filter * -Server 192.168.11.10 -Credential $Secret:DuckAdmin
 #Write-Output $Secret:DuckAdmin.Username
 #Get-ADUser -Filter * -Server 192.168.11.10
+Write-output 'toto'
